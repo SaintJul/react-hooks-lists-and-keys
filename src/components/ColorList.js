9d926a72932c31d39494b962c4ebf21d7d67380d
SaintJul <favor.julius@gmail.com>
2021-09-30 20:53:28 -0400

    commit tree 4d826a36a53a3a20614ae97609c4f7c203df6a60

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -9,14 +9,11 @@ function ColorList() {
     "hotpink",
   ];
 
+  const colorElements = colors.map((coleur) => <li key = {coleur} style ={{color: coleur}}>{coleur}</li>)
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
-      <li style={{ color: colors[0] }}>{colors[0]}</li>
-      <li style={{ color: colors[1] }}>{colors[1]}</li>
-      <li style={{ color: colors[2] }}>{colors[2]}</li>
-      <li style={{ color: colors[3] }}>{colors[3]}</li>
-      <li style={{ color: colors[4] }}>{colors[4]}</li>
+      <colorElements/>
     </div>
   );
 }
